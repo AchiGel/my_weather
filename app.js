6186f1e93d92127ec670cb930e9c3a1c8a790a71
AchiGel <ar.gelashvili@gmail.com>
2023-11-20 15:32:45 +0400

    add

--- app.js
+++ app.js
@@ -6,20 +6,36 @@ const humidity = document.querySelector(".para-1 p span");
 const visiblity = document.querySelector(".para-2 p span");
 const airPressure = document.querySelector(".para-3 p span");
 const wind = document.querySelector(".para-4 p span");
+const cityInput = document.querySelector("#cityInput");
+const findBtn = document.querySelector(".findBtn");
+const weatherCard = document.querySelector(".weather-Card");
 
 async function changeParameters() {
   const response = await fetch(
-    "https://api.weatherapi.com/v1/current.json?key=bf46b793c4064945b8c100432231711&q=Tbilisi&aqi=no"
+    `https://api.weatherapi.com/v1/current.json?key=bf46b793c4064945b8c100432231711&q=${cityInput.value}&aqi=no`
   );
   const data = await response.json();
-  city.textContent = data.location.name;
-  currentTemp.textContent = data.current.temp_c;
-  tempImg.src = data.current.condition.icon;
-  date.textContent = data.current.last_updated;
-  humidity.textContent = data.current.humidity;
-  visiblity.textContent = data.current.vis_km;
-  airPressure.textContent = data.current.pressure_mb;
-  wind.textContent = data.current.wind_kph;
+
+  try {
+    city.textContent = data.location.name;
+    currentTemp.textContent = data.current.temp_c;
+    tempImg.src = data.current.condition.icon;
+    date.textContent = data.current.last_updated;
+    humidity.textContent = data.current.humidity;
+    visiblity.textContent = data.current.vis_km;
+    airPressure.textContent = data.current.pressure_mb;
+    wind.textContent = data.current.wind_kph;
+    weatherCard.classList.add("block");
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-changeParameters();
+findBtn.addEventListener("click", changeParameters);
+
+cityInput.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    changeParameters();
+  }
+});
